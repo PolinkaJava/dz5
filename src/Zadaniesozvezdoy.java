c09bacd06deb62d8788acae72aec558c66392450
polin <paulinapytaleva@yandex.by>
2023-11-21 11:24:47 +0300

    wfcyfgh

--- src/Zadaniesozvezdoy.java
+++ src/Zadaniesozvezdoy.java
@@ -1,23 +1,23 @@
-public class zadaniesozvezdoy {
+public class Zadaniesozvezdoy {
     public static void main(String[] args) {
         int n = 9;
         int m = 9;
-        int[][] A = new int[n][m];
+        int[][] a = new int[n][m];
         int num = 1;
 
         for (int i = 0; i < n; i++) {
             if (i % 2 == 0) {
                 for (int j = 0; j < m; j++) {
-                    A[i][j] = num++;
+                    a [i][j] = num++;
                 }
             } else {
                 for (int j = m - 1; j >= 0; j--) {
-                    A[i][j] = num++;
+                    a [i][j] = num++;
                 }
             }
         }
 
-        for (int[] row : A) {
+        for (int[] row : a) {
             for (int cell : row) {
                 System.out.printf("%3d", cell);
             }
